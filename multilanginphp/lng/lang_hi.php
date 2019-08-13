485f574eddd43b2d79c57938e213e90bb9377a52
deb986 <53438118+deb986@users.noreply.github.com>
2019-08-13 16:51:38 +0530

    Add files via upload

---
+++ multilanginphp/lng/lang_hi.php
@@ -0,0 +1,9 @@
+<?php
+
+define("_REGISTER", "रजिस्टर");
+define("_NAME", "नाम");
+define("_USERNAME", "उपयोगकर्ता नाम");
+define("_EMAIL", "ईमेल");
+define("_ADDRESS", "ठिकाना");
+define("_SUBMIT", "जमा करें");
+?>
