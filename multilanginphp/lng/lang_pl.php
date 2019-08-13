36231a38866d8a5a873a674129d13f57fb0a2412
deb986 <53438118+deb986@users.noreply.github.com>
2019-08-13 16:55:01 +0530

    Delete lang_pl.php

--- multilanginphp/lng/lang_pl.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define("_REGISTER", "Zarejestrować");
-define("_NAME", "Nazwa");
-define("_USERNAME", "Nazwa Użytkownika");
-define("_EMAIL", "E-mail");
-define("_ADDRESS", "Adres");
-define("_SUBMIT", "Zatwierdź");
-?>
